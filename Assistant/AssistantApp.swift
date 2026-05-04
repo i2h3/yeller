f037cd2ef424e22a5c068f0184d2d3058b1ad819
Iva Horn <iva.horn@nextcloud.com>
2026-05-04 09:31:54 +0200

    Initial Commit

---
+++ Assistant/AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssistantApp.swift
+//  Assistant
+//
+//  Created by Iva Horn on 04.05.26.
+//
+
+import SwiftUI
+
+@main
+struct AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AssistantTests/AssistantTests.swift
@@ -0,0 +1,18 @@
+//
+//  AssistantTests.swift
+//  AssistantTests
+//
+//  Created by Iva Horn on 04.05.26.
+//
+
+import Testing
+
+struct AssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
